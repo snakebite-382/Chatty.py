243f2523fe3c9da4d6af27bf7e196952e6ed7dcf
snakebite-382 <56897435+snakebite-382@users.noreply.github.com>
2020-06-04 23:48:46 -0400

    Add the first client and server scripts
    These are the first versions of both scripts. Runs entirely in the command line, super minimalistic

---
+++ client.py
@@ -0,0 +1,57 @@
+import socket
+import select
+import errno
+import sys
+
+
+HEADER_LENGTH = 10
+
+IP = socket.gethostbyname("0.0.0.0")#'192.168.0.40' 
+PORT = 1234
+
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode("utf-8");
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+
+client_socket.send(username_header + username)
+
+while True:
+	messageInput = input(f"Me({my_username}) > ")
+	message = f"message${messageInput}"
+
+	if(message):
+		message = message.encode("utf-8")
+		message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+		client_socket.send(message_header + message)
+		print(f"sent {message_header} : {message}")
+
+	try:
+		while True:
+			username_header = client_socket.recv(HEADER_LENGTH)
+			if(not len(username_header)):
+				print("connection closed by server")
+				sys.exit()
+			username_lenght = int(username_header.decode("utf-8").strip())
+
+			username = client_socket.recv(username_lenght).decode("utf-8")
+
+			message_header = client_socket.recv(HEADER_LENGTH)
+			message_length = int(message_header.decode("utf-8").strip())
+			messageRaw = client_socket.recv(message_length).decode("utf-8")
+			type_, message = messageRaw.split("$")
+			if(type_ == message):
+				print(f"{username} >> {message}")
+
+	except IOError as e:
+		if(e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK):
+			print("READ ERR",str(e))
+			sys.exit()
+		continue
+
+	except Exception as e:
+		print("Error".str(e))
+		sys.exit()
---
+++ server.py
@@ -0,0 +1,78 @@
+import socket
+import select
+
+HEADER_LENGTH = 10
+IP = socket.gethostbyname('0.0.0.0')
+PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+print("server started")
+
+def recieve_message(client_socket):
+	message_header = client_socket.recv(HEADER_LENGTH)
+
+	if not len(message_header):
+		return False
+
+	message_length = int(float(message_header.decode('utf-8').strip()))
+	return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+while True:
+	read_sockets, _x_, exception_sockets = select.select(sockets_list, [], sockets_list) 
+	
+	for notified_socket in read_sockets:
+		if notified_socket == server_socket:
+			client_socket, client_address = server_socket.accept()
+			print("accepted")
+
+			user = recieve_message(client_socket)
+			
+			print(user)
+			print("Recieved")
+			
+			if(not user):
+				continue
+
+			sockets_list.append(client_socket)
+			print("added to list")
+
+			clients[client_socket] = user
+			print("created user")
+
+			print(f"Accepted connection from {client_address[0]}{client_address[1]} username: {user['data'].decode('utf-8')}")
+
+		else:
+			message = recieve_message(notified_socket)
+
+			if(message is False):
+				print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+				sockets_list.remove(notified_socket)
+				del clients[notified_socket]
+				continue
+
+			else:
+			 	user = clients[notified_socket]
+
+			 	type_, data = message['data'].decode("utf-8").split("$")
+
+
+			 	if(type_ == "message"):
+				 	print(f"Recieved Message from {user['data'].decode('utf-8')} : {message['data'].decode('utf-8')} of type {type_}")
+				 	for client_socket in clients:
+
+				 		if client_socket != notified_socket:
+				 			client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+	for notified_socket in exception_sockets:
+		sockets_list.remove(notified_socket)
+		del clients[notified_socket]	
+		print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}") 			
